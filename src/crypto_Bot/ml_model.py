56c347f8ed6ef4eb883473be4721dbe197975768
manoa <manoa9438@gmail.com>
2024-08-23 16:52:22 +0200

    create env and create connection for form and ml model

--- src/crypto_Bot/ml_model.py
+++ src/crypto_Bot/ml_model.py
@@ -1,7 +1,7 @@
 from crypto_Bot.models import Klines
 
 
-def result_ml():
+def result_ml(model_choice, interval_min, candlestick_count):
+    breakpoint()
     # récupère tout les Klines de la bdd
-    data_klines = Klines.objects.all()
     return "result Beautifull ml"
--- src/crypto_Bot/views/views.py
+++ src/crypto_Bot/views/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import viewsets
 
@@ -6,11 +7,25 @@ from crypto_Bot.serializer import KlinesListSerializer
 from crypto_Bot.ml_model import result_ml
 
 
+from django.shortcuts import render
+
 def index(request):
-    all_kines = Klines.objects.all()
-    # Exécuter la fonction result_ml et récupérer son résultat
-    result_ml_test = result_ml()
-    return render(request, 'index.html', context={'all_kines': all_kines, 'result_ml_test': result_ml_test})
+    if request.method == "POST":
+        # Récupère les données du formulaire
+        model_choice = request.POST.get('model_choice')
+        interval_min = request.POST.get('interval_min')
+        candlestick_count = request.POST.get('candlestick_count')
+
+        # Vérifie si toutes les données sont présentes
+        if model_choice and interval_min and candlestick_count:
+            # Appeler la fonction ML avec les trois paramètres
+            result = result_ml(model_choice, interval_min, candlestick_count)
+            return render(request, 'index.html', {})
+        else:
+            return render(request, 'index.html', {'error': 'Please fill out all fields.'})
+    else:
+        return render(request, 'index.html')
+
 
 
 #expose sous forme d'api ma bdd
